4d26a6b104d9321b98208cf63451046fc6070e4a
gururajtakale <gururajtakale0007@gmail.com>
2026-02-10 00:12:34 +0530

    Updated Api And AuhService

--- goswift-frontend/src/services/auth.service.js
+++ goswift-frontend/src/services/auth.service.js
@@ -1,9 +1,11 @@
 import api from "./api";
 
+
 const login = async (email, password) => {
+
   const response = await api.post("/auth/login", { email, password });
   const { token, user } = response.data.data;
-  
+
   // Store token and user data
   localStorage.setItem("token", token);
   localStorage.setItem("user", JSON.stringify(user));
@@ -11,22 +13,36 @@ const login = async (email, password) => {
   return { token, user };
 };
 
+
 const signup = async (userData) => {
+
   const response = await api.post("/auth/signup", userData);
   return response.data.data;
 };
 
+
 const logout = () => {
+
   localStorage.removeItem("token");
   localStorage.removeItem("user");
 };
+
 
 const getCurrentUser = () => {
   return JSON.parse(localStorage.getItem("user"));
 };
 
+
 const getToken = () => {
   return localStorage.getItem("token");
 };
 
+
 export default { login, signup, logout, getCurrentUser, getToken };
+
+
+
+
+
+
+
